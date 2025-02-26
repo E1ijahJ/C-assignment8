00aab29c9b2b2b3aeef76fa7cd5072b840ddf9fa
E1ijahJ <elijahguijoseph@gmail.com>
2025-02-25 22:05:53 -0500

    Created Person Class under people

--- AdvLINQ/People.cs
+++ AdvLINQ/People.cs
@@ -1,1 +1,16 @@
-﻿
+﻿namespace AdvLINQ;
+
+public class People
+{
+private class Person{
+  public int Id {get; set;}
+  public string Name{ get; set;}
+  public DateTime Birthday {get; set;}
+  public Person(int id, string name, DateTime birthday){
+
+    Id=id;
+    Name=name;
+    Birthday=birthday;
+  }
+}
+}
